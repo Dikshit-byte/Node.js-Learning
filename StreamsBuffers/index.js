25d5b29d39d5ef1a061d22e5cda950a084ebf946
Dikshit <singhdikshit69@gmail.com>
2022-07-23 10:46:28 +0530

    add streambuffers

--- StreamsBuffers/index.js
+++ StreamsBuffers/index.js
@@ -13,3 +13,41 @@
 // end - This event is fired when there is no more data to read.
 // error - This event is fired when there is any error receiving or writing data.
 // finish - This event is fired when all the data has been flushed to underlying system.
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+server.on("request",(req,res)=>{
+    // fs.readFile("input.txt",(err,data)=>{
+    //     if(err) console.error(err);
+    //     res.end(data.toStringfy());
+    // })
+
+// 2nd way 
+// Reading from a stream
+// Create a readable stream
+// Handle stream events -> data,end, and error.
+
+// const rstream = fs.createReadStream('input.txt');
+
+// rstream.on('data',(chunkdata)=>{
+//     res.write(chunkdata);
+// })
+// rstream.on('end',()=>{
+//     res.end();
+// })
+// rstream.on('error',(err)=>{
+//     console.error(err);
+// })
+
+    // 3rd way
+
+const rstream = fs.createReadStream("input.txt");
+rstream.pipe(res);
+
+});
+server.listen(3000,"127.0.0.1",()=>{
+    console.log("Listening to the port no 3000");
+})
